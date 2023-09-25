65fc9fe6f8084d407307fc111fc9045fe981ec3c
Tony DeVere <tdevere@microsoft.com>
2023-09-25 16:28:52 -0500

    Add project files.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start("f83e4985-1ca5-4d95-801a-116db24446a6",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+    }
+
+}
